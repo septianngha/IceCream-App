ce7e60b69c3ba95a6f3f3c44050dee12e88242b4
Septian Nugraha <septiannugraha341@gmail.com>
2024-11-02 05:54:06 +0700

    Initial Commit

---
+++ Ice Cream App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ice Cream App
+//
+//  Created by Muhamad Septian Nugraha on 02/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
